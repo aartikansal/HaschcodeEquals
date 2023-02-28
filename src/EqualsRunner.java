435223d36b16ea7668465702d6832738b25704d1
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-28 10:56:30 -0500

    Initial commit

---
+++ src/EqualsRunner.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class EqualsRunner {
+    static class Client{
+        private int id;
+
+        public Client(int id) {
+            super();
+            this.id = id;
+        }
+        //equals
+        //hashcode
+
+        @Override
+        public boolean equals(Object that) {
+            if (this == that) return true;
+            if (that == null || getClass() != that.getClass()) return false;
+            Client client = (Client) that;
+            return id == client.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+    public static void main(String[] args) {
+        Client c1= new Client(1);
+        Client c2 = new Client(1);
+        Client c3 = new Client(2);
+        System.out.println(c1.equals(c2));
+        System.out.println(c1.equals(c3));
+
+    }
+}
