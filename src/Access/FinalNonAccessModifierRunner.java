47c3321545c356916803a1be4b7da3dd56028bef
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-03-01 17:32:05 -0500

    Finalmethod comment

--- src/Access/FinalNonAccessModifierRunner.java
+++ src/Access/FinalNonAccessModifierRunner.java
@@ -4,6 +4,7 @@ final class FinalClass{
 }
 //class SomeClass extends FinalClass----FINAL CLASS CANNOT BE EXTEND
 //whenever you do not want to extend any class, not allow any changes
+//you cannot override the final method
 
 public class FinalNonAccessModifierRunner {
     public static void main(String[] args) {
