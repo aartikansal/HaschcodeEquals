8082a89bd678c45eebb046b99f95d5c69596d92e
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-03-01 17:29:59 -0500

    FinalClass

---
+++ src/Access/FinalNonAccessModifierRunner.java
@@ -0,0 +1,11 @@
+package Access;
+final class FinalClass{
+
+}
+//class SomeClass extends FinalClass----FINAL CLASS CANNOT BE EXTEND
+//whenever you do not want to extend any class, not allow any changes
+
+public class FinalNonAccessModifierRunner {
+    public static void main(String[] args) {
+    }
+}
